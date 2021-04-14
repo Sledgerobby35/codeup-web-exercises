fc57431f42752646247fb80ea258c547d6ec8549
Robert Sledge <robby@Roberts-MBP.lan>
2021-04-14 12:42:10 -0500

    Exercise Update

---
+++ js/external.js
@@ -0,0 +1,9 @@
+"use strict";
+alert("Welcome to my website!");
+var usersColor = prompt("What is your favorite color?");
+alert("Your favorite color is " + usersColor);
+if (usersColor.toLowerCase() === "red") {
+    alert("That's really cool, my favorite color also happens to be red");
+} else {
+    alert("Your favorite color is " + usersColor);
+}
