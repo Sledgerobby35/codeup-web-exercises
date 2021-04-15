61e3a4d48807c2e67478255937b92f525f9f0012
Robert Sledge <robby@Roberts-MBP.lan>
2021-04-15 15:11:12 -0500

    adds operators exercises

---
+++ js/operators-exercises.js
@@ -0,0 +1,18 @@
+"use strict";
+console.log('1' + 2); //3
+console.log(typeof('1' + 2)); //number
+console.log(6%4); //2
+console.log('3 + 4 is ' + 3 + 4); //error
+console.log(0 < 0);
+console.log('false' == false); //true
+console.log(true == 'true'); //true
+console.log(5 >= -5); //true
+console.log(!false || false); //true
+console.log(true || "42"); //true
+console.log(6 % 5); //1
+console.log(5 < 4 && 1 === 1); //false
+console.log(typeof('codeup') === 'string'); //true
+console.log('codeup' === 'codeup' && 'codeup' === 'Codeup'); //false
+console.log(4 >= 0 && 1 !== '1'); //false
+console.log(6 % 3 === 0); //true
+console.log(5 % 2 !== 0); //true
