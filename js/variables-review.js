ba1734aa1d08b4e9d67cb283b3317385c5f82157
Robert Sledge <robby@Roberts-MBP.lan>
2021-04-15 14:32:11 -0500

    adds variables-review class notes

---
+++ js/variables-review.js
@@ -0,0 +1,40 @@
+// =========== VARIABLES ===========
+
+// Know about the purpose of variables in math?
+
+// Yeah, it's the same idea here!
+
+// A variable is a symbolic representative of a value that can be different in each run of a function
+
+// What good is data if we don't have a way to pass around and use it in various ways?
+
+// Introducing: variables!.... again!
+
+// you have (in this order, left to right):
+// the type of variable,
+// the name of the variable,
+// the assignment operator (=),
+// and the value (in this case, a number)
+//var num = 5; // now we have a variable we can use in other places!
+
+// By declaring a variable, we are commanding the runtime (our language's engine for actually running the program)
+// to reserve a bit of memory (RAM) for the value stored in said variable.'
+
+// Well, how can we use it?
+// Let's call a function (yes, still think in the math sense) which will use it to do something
+
+// In this case, print it out to the user's console:
+//console.log(num);
+
+//Mini Exercise
+// console.log(9);
+// console.log(7);
+// console.log(10);
+// console.log("Hello");
+// console.log("Codeup");
+// console.log("Students");
+// console.log(true);
+// console.log(false);
+// console.log(!true);
+var num;
+console.log(num);
