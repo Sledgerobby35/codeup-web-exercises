e3c9f6b6ff9538339724743aa1377886e5d1bb9d
Robert Sledge <robby@Roberts-MBP.lan>
2021-04-15 14:56:10 -0500

    adds primitive types exercises, plus file name change

---
+++ js/primitive-types-exercises.js
@@ -0,0 +1,15 @@
+"use strict";
+//Number occupy 64-bit memory, Double-precision floating-point
+console.log(typeOf(99.9));
+console.log(typeOf("false"));
+console.log(typeOf(false));
+console.log(typeOf('0'));
+console.log(typeOf(0));
+console.log(typeOf(true));
+console.log(typeOf('true'));
+//A term or phrase typed into a search box? Answer: string
+//If a user is logged in? Answer: boolean
+//A discount amount to apply to a user's shopping cart? Answer: number
+//Whether or not a coupon code is valid? Answer: boolean
+//An email address typed into a registration form? Answer: number
+//The price of a product? Answer: number
