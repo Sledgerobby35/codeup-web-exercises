0e87e97836000a6c3355dad89836ea2290dbe412
Robert Sledge <robby@Roberts-MBP.lan>
2021-04-15 17:04:25 -0500

    adds shorthand, unary, and jsmethods class notes

---
+++ js/shorthand-unary-and-jsmethods.js
@@ -0,0 +1,84 @@
+"use strict";
+
+// SHORTHAND ASSIGNMENT OPERATORS
+//
+// // += / -=
+//
+// var name ="Robert";
+//
+// //name = name + " " + "Sledge";
+// //console.log(name);
+//
+// name += " " + "Sledge";
+// //console.log(name);
+//
+// //console.log(name -= "Sledge");
+//
+// var daysRemainingInCohort = 114;
+//
+// daysRemainingInCohort -= 4;
+// //console.log(daysRemainingInCohort);
+//
+//
+// //Mini Exercise
+// var result1 = 2;
+// var result2 = 8;
+// var result3 = 2;
+// var result4 = 6;
+// var result5 = 14;
+// // console.log(result1 * 2);
+// // console.log(result2 / 2);
+// // console.log(result3 + 2);
+// // console.log(result4 - 2);
+// // console.log(result5 % 5);
+// result1 *= 2;
+// console.log(result1);
+// result2 /= 2;
+// console.log(result2);
+// result3 += 2;
+// console.log(result3);
+// result4 -= 2;
+// console.log(result4);
+// result5 %= 5;
+// console.log(result5);
+
+// UNARY
+
+// var makeItANumber = "doge";
+// console.log(typeof(makeItANumber));
+// console.log(typeof(+makeItANumber));
+
+// INCREMENT/DECREMENT
+
+//post-increment (num++) decrement (num--)
+
+// var num = 10;
+// console.log(num++);
+// console.log(num);
+
+//JS BUILT-IN METHODS
+
+//toString();
+// var num = 5 * 5;
+// console.log(typeof num.toString());
+
+// var pi = 3.1415926535;
+// console.log(pi.toFixed(5));
+
+//Number()
+
+// console.log(Number("1234"));
+//
+// //PAY ATTENTION TO METHODS AND HOW USED
+//
+// // isNaN() - checks to see if value provided is Not-a-Number
+//
+// console.log(!isNaN("Robert"));
+//
+// // parseInt()
+//
+// console.log(parseInt("809 GardenMeadow"));
+
+//.length - PROPERTY
+
+console.log("Robert".length);
