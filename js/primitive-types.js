0ac079afa80e995bd414aa2b23eca1f94a87741b
Robert Sledge <robby@Roberts-MBP.lan>
2021-04-15 14:30:47 -0500

    adds primitive-types class notes

---
+++ js/primitive-types.js
@@ -0,0 +1,9 @@
+"use strict";
+//Number occupy 64-bit memory, Double-precision floating-point
+console.log(typeOf(99.9));
+console.log(typeOf("false"));
+console.log(typeOf(false));
+console.log(typeOf('0'));
+console.log(typeOf(0));
+console.log(typeOf(true));
+console.log(typeOf('true'));
