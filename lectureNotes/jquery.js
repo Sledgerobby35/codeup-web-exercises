76497d5dfced077cd5095e956ad27528d3178a91
Sledgerobby35 <sledge.robby35@gmail.com>
2021-06-01 23:45:46 -0500

    add js file for lecture notes on jquery

---
+++ lectureNotes/jquery.js
@@ -0,0 +1,15 @@
+"use strict"
+// $(document).ready(function(){
+// 	$("#all-caps").hide();
+// 	$("span").hide();
+// 	$(".show-me").show();
+// });
+
+$(".pink").css({"background-color": "pink"});
+$(".orange").css({"background-color": "orange"});
+$(window).load(function(){
+	$(".pink").hide();
+});
+$(document).ready(function(){
+	$(".orange").hide();
+});
