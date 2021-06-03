d9853307fe621791e1be02af160a84f7e8ea2e3a
Sledgerobby35 <sledge.robby35@gmail.com>
2021-06-02 19:31:21 -0500

    add js file for jquery exercises

--- js/jquery_exercises.js
+++ js/jquery_exercises.js
@@ -1,4 +1,16 @@
 "use strict";
-$(document).ready(function(){
-	alert("The DOM has finished loading");
-});
+// $(document).ready(function(){
+// 	alert("The DOM has finished loading");
+// });
+// let pizzaContent = $("#pizza").html();
+// alert(pizzaContent);
+// let siopaoContent = $("#siopao").html();
+// alert(siopaoContent);
+// let listContent = $("#list-item").html();
+// alert(listContent);
+// $(".codeup").css("border", "1px solid red");
+$("li").css("font-size", "20px");
+// $("h1").css("background-color", "rgba(255,255,102,0.5)");
+// $("p").css("background-color", "rgba(255,255,102,0.5)");
+// $("li").css("background-color", "rgba(255,255,102,0.5)");
+$("h1, p, li").css("background-color", "rgba(255,255,102,0.5)");
