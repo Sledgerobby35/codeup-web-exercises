d8f988081d1d69436155402e5785ae3e109ea67d
Sledgerobby35 <sledge.robby35@gmail.com>
2021-06-09 12:41:20 -0500

    add mapbox js file

---
+++ lectureNotes/mapboxPlayground.js
@@ -0,0 +1,50 @@
+const mapboxToken = 'pk.eyJ1IjoicmVsb3J0MzUiLCJhIjoiY2twcG9kdmMwMjlzMzMxbnk2a3IxbTM3eiJ9.QXAAIgqF4AJgT2TAURwGsw';
+ mapboxgl.accessToken = mapboxToken;
+
+ var map = new mapboxgl.Map( {
+	 container: 'map',
+ 	style: 'mapbox://styles/mapbox/satellite-streets-v11',
+	 center: [-97.1081, 32.7357],
+	 zoom: 12
+ });
+
+ let marker = setMarker([-135.7460400, 35.4957600]);
+ addMapEvent(marker);
+ let geocoder = setGeocoder();
+addGeocoderToMap(geocoder);
+addGeocoderEvent(geocoder);
+setPopup("This is my popup! Woo!");
+ function setGeocoder(){
+ 	return new MapboxGeocoder({
+		accessToken: mapboxgl.accessToken,
+		mapboxgl: mapboxgl,
+		marker: false
+	})
+ }
+function addGeocoderToMap(geocoder){
+ 	map.addControl(geocoder);
+}
+
+function addGeocoderEvent(geocoder){
+ 	geocoder.on("result", function(event){
+ 		marker.setLngLat(event.result);
+ 		setPopup(e.result.resultPlace_name);
+	})
+}
+ function setMarker(point) {
+ 	return new mapboxgl.Marker().setLngLat(point).addTo(map)
+ }
+function addMapEvent(marker) {
+ 	map.on('click', function (event) {
+ 		console.log(event.lngLat);
+ 		marker.setLngLat(event.lngLat).addTo(map);
+	})
+}
+
+function setPopup(textDetails){
+ 	let popup = new mapboxgl.Popup()
+		.setHTML(`<p>${textDetails}</p>`)
+		.addTo(map);
+
+ 	marker.setPopup(popup);
+}
