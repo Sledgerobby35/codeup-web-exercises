8367a3ecd6f2cee2334037b89ea2f245c869a56d
Sledgerobby35 <sledge.robby35@gmail.com>
2021-06-11 17:46:54 -0500

    add js file for mapboxPlayground

--- lectureNotes/mapboxPlayground.js
+++ lectureNotes/mapboxPlayground.js
@@ -11,9 +11,11 @@ const mapboxToken = 'pk.eyJ1IjoicmVsb3J0MzUiLCJhIjoiY2twcG9kdmMwMjlzMzMxbnk2a3Ix
  let marker = setMarker([-135.7460400, 35.4957600]);
  addMapEvent(marker);
  let geocoder = setGeocoder();
+
 addGeocoderToMap(geocoder);
-addGeocoderEvent(geocoder);
-setPopup("This is my popup! Woo!");
+// console.log(addGeocoderEvent(geocoder));
+// setPopup("This is my popup! Woo!");
+
  function setGeocoder(){
  	return new MapboxGeocoder({
 		accessToken: mapboxgl.accessToken,
@@ -28,15 +30,17 @@ function addGeocoderToMap(geocoder){
 function addGeocoderEvent(geocoder){
  	geocoder.on("result", function(event){
  		marker.setLngLat(event.result);
- 		setPopup(e.result.resultPlace_name);
+		console.log(event);
+		 return event.result.place_name;
 	})
+
 }
+// setPopup(addGeocoderEvent(geocoder));
  function setMarker(point) {
  	return new mapboxgl.Marker().setLngLat(point).addTo(map)
  }
 function addMapEvent(marker) {
  	map.on('click', function (event) {
- 		console.log(event.lngLat);
  		marker.setLngLat(event.lngLat).addTo(map);
 	})
 }
